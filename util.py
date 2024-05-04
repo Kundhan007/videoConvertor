9b5ea38455cea7cb176da9c93a170ec3ae54cfd1
kundhan <kundhan1997@gmail.com>
2024-05-04 22:21:56 +0530

    config file tested

--- util.py
+++ util.py
@@ -13,12 +13,6 @@ def files_in_folder(input_folder):
     return files
 
 
-int_folder = ("D:\maid movie\harry potter\www.TamilBlasters.ws - Harry Potter Octalogy (2001 to 2011)[720p - BDRip's - "
-              "[Tamil + Telugu + Hindi + Eng] - x264 - 14GB]")
-
-print(*files_in_folder(int_folder), sep='\n')
-
-
 def load_config():
     config = configparser.ConfigParser()
     config.read('config.ini')
